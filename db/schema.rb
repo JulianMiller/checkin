579cbb943f98c94df2046a00a43ebcedf004a4ba
Tomer Elmalem <telmalem@gmail.com>
2011-12-23 16:15:20 -0500

    Migrate the events.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111222180447) do
+ActiveRecord::Schema.define(:version => 20111223211327) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.date     "date"
+    t.time     "time"
+    t.string   "timezone"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
