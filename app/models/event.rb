f470821c1dd0c420834bd2e5594b9dd1b879c9f5
Tomer Elmalem <telmalem@gmail.com>
2011-12-28 15:38:36 -0500

    Add the timezone from the form to the datetime object.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,13 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  attr_accessor :timezone
+
+  before_save :add_timezone
+
+  def add_timezone
+    Time.zone = timezone
+    tz = Time.zone.utc_offset/60/60
+    datetime.change(:offset => tz.to_s)
+  end
 end
