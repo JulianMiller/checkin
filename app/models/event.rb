219ce265d97b5b17aa930cb0468c16159202685a
Tomer Elmalem <telmalem@gmail.com>
2012-01-02 14:04:10 -0500

    Convert the time to UTC before saving it.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,19 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  before_save :to_utc
+
+  def to_utc
+    # Set the timezone
+    Time.zone = timezone
+
+    # Get the offset in hours
+    offset = Time.zone.utc_offset/60/60
+
+    # Invert the sign of the offset to we can get it back to UTC
+    offset *= -1
+
+    # Store the time in UTC
+    self.datetime += offset.hours
+  end
 end
