8f81bc878a86806dff56d99ee9faa273fa1655f9
Tomer Elmalem <telmalem@gmail.com>
2012-01-05 15:17:45 -0500

    Create the attendee table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111228212658) do
+ActiveRecord::Schema.define(:version => 20120105201633) do
+
+  create_table "attendees", :force => true do |t|
+    t.integer  "event_id"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "events", :force => true do |t|
     t.string   "name"
