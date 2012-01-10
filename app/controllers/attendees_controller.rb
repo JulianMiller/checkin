aa4516f0a8687cf8fdf36cb6573cff51356048f5
Tomer Elmalem <telmalem@gmail.com>
2012-01-10 17:24:08 -0500

    Send an email after the attendee has submitted their email.

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -3,5 +3,7 @@ class AttendeesController < ApplicationController
   end
 
   def create
+    GiftMailer.send_gift(params[:email]).deliver
+    redirect_to root_path, notice: 'An email should be sent to you soon.'
   end
 end
