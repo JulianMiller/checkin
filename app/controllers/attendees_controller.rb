2cceb367d516bf1dcc57506e28a5f17e7fce83ba
Tomer Elmalem <telmalem@gmail.com>
2012-01-10 16:54:45 -0500

    Generate controller files needed for sending an email.

---
+++ app/controllers/attendees_controller.rb
@@ -0,0 +1,7 @@
+class AttendeesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Checkin::Application.routes.draw do
+  get "attendees/new"
+  post "attendees/create"
+
   resources :events, :attendees
   devise_for :users
 
