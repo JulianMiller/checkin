08a571fb684b1b619477cd18f6631c065d49246b
Tomer Elmalem <telmalem@gmail.com>
2012-01-11 21:14:19 -0500

    Initialize google maps on the new event page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,22 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function initialize() {
+  var myOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(33.748995, -84.387982),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+function loadScript() {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBQSkqRMekudpB3Ep_iNJhp27YevAUtW3Q&sensor=false&callback=initialize";
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
